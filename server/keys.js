0d84476561ca2197d3a1441fe835d8f33983d439
Sonia-ttn <99384358+Sonia-ttn@users.noreply.github.com>
2022-04-22 11:51:58 +0530

    Update keys.js

--- server/keys.js
+++ server/keys.js
@@ -1,4 +1,5 @@
 module.exports={
     URI:"mongodb://0.0.0.0:27017/playground",
-    JWT_SECRET_KEY:"Asw.&mU7T$=SbW3]q8C["
+    JWT_SECRET_KEY:"Asw.&mU7T$=SbW3]q8C[",
+     CLIENT_ID:"905525222638-vpb75i5ilgu84vj74dr1d484rg2q4mt9.apps.googleusercontent.com"
 }
