26cbf3409d583386fb3ba4688db6ebd91e846765
Sonia-ttn <99384358+Sonia-ttn@users.noreply.github.com>
2022-04-25 18:51:42 +0530

    Update keys.js

--- server/keys.js
+++ server/keys.js
@@ -4,5 +4,5 @@ module.exports={
      CLIENT_ID:"905525222638-vpb75i5ilgu84vj74dr1d484rg2q4mt9.apps.googleusercontent.com"
     
     CLIENT_URL:"https://localhost:3000",
-    API_KEY:"defc86454cc63f1c74deb254e50a0da6-02fa25a3-2ba2d983"
+    
 }
