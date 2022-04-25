adfb9269d8fa2792fefbe40cba7ab4e1abff89ce
Sonia-ttn <99384358+Sonia-ttn@users.noreply.github.com>
2022-04-25 18:39:29 +0530

    forgot password frontend

--- client/src/components/forgotpassword/index.jsx
+++ client/src/components/forgotpassword/index.jsx
@@ -1,11 +1,82 @@
-import React from 'react'
 
-function index() {
-  return (
-    <div>
+import styles from "./style.module.css";
+import { useState} from "react";
+import axios from "axios";
+import React from 'react';
+ 
+// Importing toastify module
+import {toast} from 'react-toastify';
+ 
+// Import toastify css file
+import 'react-toastify/dist/ReactToastify.css';
+
+import { useNavigate } from "react-router-dom";
+ 
+ // toast-configuration method,
+ // compulsory one.
+toast.configure()
+
+
+function Forgot() {
+  const [data, setData] = useState({ email: "" });
+  const [error, setError] = useState("");
+
+  const handleChange = ({ currentTarget: input }) => {
+    setData({ ...data, [input.name]: input.value });
+  };
+  const navigate = useNavigate();
+  
+  const submit=async(e)=>{
+   
+    e.preventDefault();
+    try {
       
+      const url = "http://localhost:5000/forgot-password";
+      const { data: res } = await axios.put(url, data);
+      //response from backend
+      console.log("Successfull msg",res);
+      toast('Check your email',
+           {position: toast.POSITION.TOP_CENTER})
+      
+     // navigate("/login")
+     
+    } 
+    catch (error) {
+      if (error.response &&
+        error.response.status >= 400 &&
+        error.response.status <= 500) {
+      setError(error.response.data.message);}
+     
+
+    }
+  };
+
+  return (
+    <div className={styles.login_container}>
+
+      <form className={styles.login_form_container} onSubmit={submit}>
+
+        <h1>Forgot Password</h1>
+        
+        <input
+          type="email"
+          placeholder="Enter Your Email"
+          name="email"
+          value={data.email}
+          onChange={handleChange}
+          required
+          className={styles.input} 
+        />
+        
+        {error && <div className={styles.error_msg}>{error}</div>}
+
+        <button type="submit"className={styles.green_btn} >Send Reset Link</button>
+      
+
+      </form>
+
     </div>
   )
-}
+  };
 
-export default index
+export default Forgot;
