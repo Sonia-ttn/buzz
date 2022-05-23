be61b0c4d0b766c1e9510114d18f753eb12db463
Sonia-ttn <99384358+Sonia-ttn@users.noreply.github.com>
2022-05-23 12:19:04 +0530

    Update keys.js

--- server/keys.js
+++ server/keys.js
@@ -1,5 +1,9 @@
 module.exports={
-    URI:"mongodb://0.0.0.0:27017/playground",
+    URI:"mongodb+srv://Sonia:47FkXXAigFe3IEjJ@cluster0.xwf70.mongodb.net/playground?retryWrites=true&w=majority",
+   
+    CLIENT_ID:"905525222638-vpb75i5ilgu84vj74dr1d484rg2q4mt9.apps.googleusercontent.com",
+    
+    CLIENT_URL:"https://localhost:3000",
     JWT_SECRET_KEY:"Asw.&mU7T$=SbW3]q8C[",
-     CLIENT_ID:"905525222638-vpb75i5ilgu84vj74dr1d484rg2q4mt9.apps.googleusercontent.com"
+    API_KEY:""
 }
